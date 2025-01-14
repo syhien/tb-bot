9711e2e43d2f63e4359652c3cbc6d9e8109d66cc
syhien <syhien@outlook.at>
2025-01-14 21:51:31 +0800

    成功实现了对话中穿插聊天

--- wxbot.py
+++ wxbot.py
@@ -37,7 +37,6 @@ def taobao_search(message, session):
 """你会判断用户的消息是否谈及了商品。如果用户提到了商品，尝试提取消息中的商品标题部分并进行商品搜索。当用户的消息不包含商品标题时，你会通过对话向用户提供需要的帮助。
 为了绕过淘宝联盟对于精确搜索的限制，你可以在不影响搜索准确性的前提下，交换商品标题中关键词的顺序。但你不能进行可能导致搜索结果不准确的修改。
 """}] + history
-    print(messages)
     response = gemini2.chat.completions.create(
         model="gemini-2.0-flash-exp",
         messages=messages,
@@ -47,7 +46,7 @@ def taobao_search(message, session):
     if response.choices[0].message.tool_calls is not None:
         title = json.loads(response.choices[0].message.tool_calls[0].function.arguments)["title"]
         result, _ = taobao(title)
-        session["history"] = []
+        session["history"] = history + [{"role": "assistant", "content": "gemini2.0flash认为商品标题是【{}】，搜索结果如下：\n{}".format(title, result)}]
         return "gemini2.0flash认为商品标题是【{}】，搜索结果如下：\n{}".format(title, result)
     else:
         session["history"] = history + [{"role": "assistant", "content": response.choices[0].message.content}]
